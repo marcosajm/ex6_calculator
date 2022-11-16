611897c8d6f39d7d8dc10f36c8ea674b281b7828
marcos <ilejbr@gmail.com>
2022-11-16 12:54:46 +0000

    version 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,56 @@
-public class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        char operator;
+        Double number1, number2, result;
+
+        // create an object of Scanner class
+        Scanner input = new Scanner(System.in);
+
+        // ask users to enter operator
+        System.out.println("Escolha operador: +, -, *, or /");
+        operator = input.next().charAt(0);
+
+        // ask users to enter numbers
+        System.out.println("Primeiro número");
+        number1 = input.nextDouble();
+
+        System.out.println("Segundo número");
+        number2 = input.nextDouble();
+
+        switch (operator) {
+
+            // performs addition between numbers
+            case '+':
+                result = number1 + number2;
+                System.out.println(number1 + " + " + number2 + " = " + result);
+                break;
+
+            // performs subtraction between numbers
+            case '-':
+                result = number1 - number2;
+                System.out.println(number1 + " - " + number2 + " = " + result);
+                break;
+
+            // performs multiplication between numbers
+            case '*':
+                result = number1 * number2;
+                System.out.println(number1 + " * " + number2 + " = " + result);
+                break;
+
+            // performs division between numbers
+            case '/':
+                result = number1 / number2;
+                System.out.println(number1 + " / " + number2 + " = " + result);
+                break;
+
+            default:
+                System.out.println("Invalido operador!");
+                break;
+        }
+
+        input.close();
     }
 }
